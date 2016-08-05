a1a17022a51765fcd61a386e356486787aabb690
Deepa Ramam <dshenoy@gmail.com>
2016-08-05 13:10:02 -0700

    more tooling fixes

--- internals/scripts/createJSDir.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var exec = require('child_process').exec;
-
-// If this is the first build, run tsc (typescript compiler) so the
-// ts files are compiled to the js directory
-if (!fs.existsSync('js')) {
-  exec('tsc');
-} 
